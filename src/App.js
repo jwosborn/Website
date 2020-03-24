9ff598982969edc40f8629ab59b899bc24dcc3f6
jwosborn <osbornjeremyw@yahoo.com>
2020-03-24 17:52:13 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Beginnings from "./components/beginnings.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="router-wrapper">
+        <Router>
+          <div className="nav-wrapper">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/beginnings">Beginnings</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <Switch>
+            <Route path="/"></Route>
+            <Route path="/beginnings">
+              <Beginnings />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ src/components/beginnings.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Beginnings() {
+  return (
+    <div className="beg-wrapper">
+      <h1>Beginnings</h1>
+    </div>
+  );
+}
+export default Beginnings;
