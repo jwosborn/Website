068d67b5546a5595122f49da8168b725542b195d
jwosborn <osbornjeremyw@yahoo.com>
2020-03-31 11:34:27 -0400

    beginnings slides start

--- src/components/beginnings.js
+++ src/components/beginnings.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import Childhood from "../components/Beginnings/Childhood";
 import HighSchool from "../components/Beginnings/HighSchool.js";
 
-function Beginnings() {
-  let displayTile = "childhood";
-  function toggleDisplay() {
-    // Timeout here
-  }
-  return (
-    <div>
-      <div className="hero">
-        <span className="hero-body">
-          <p className="title underline">Beginnings</p>
-        </span>
+class Beginnings extends Component {
+  state = {
+    i: 0
+  };
+
+  viewsIncrement = () => {
+    this.setState({
+      i: this.state.i + 1
+    });
+  };
+  viewsDecrement = () => {
+    this.setState({
+      i: this.state.i - 1
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div className="hero">
+          <span className="hero-body">
+            <p className="title underline">Beginnings</p>
+          </span>
+        </div>
+        {this.state.i === 0 ? <Childhood /> : ""}
+        {this.state.i === 1 ? <HighSchool /> : ""}
+        <div className="buttons ">
+          {this.state.i >= 1 ? (
+            <div className="button  next-view" onClick={this.viewsDecrement}>
+              &lt;-
+            </div>
+          ) : (
+            ""
+          )}{" "}
+          {this.state.i <= 3 ? (
+            <div className="button prev-view" onClick={this.viewsIncrement}>
+              -&gt;
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
-      {displayTile === "childhood" ? <Childhood /> : <HighSchool />}
-    </div>
-  );
+    );
+  }
 }
 export default Beginnings;
