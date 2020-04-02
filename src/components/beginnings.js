dbd1eaa0a1ac137e54645dee360fbb8552f65e36
jwosborn <osbornjeremyw@yahoo.com>
2020-04-02 14:35:47 -0400

    buttons to top/column fixes

--- src/components/beginnings.js
+++ src/components/beginnings.js
@@ -29,32 +29,56 @@ class Beginnings extends Component {
       return "Grad School";
     }
   }
+  prevView() {
+    if (this.state.i === 1) {
+      return "Childhood";
+    } else if (this.state.i === 2) {
+      return "High School";
+    } else if (this.state.i === 3) {
+      return "College";
+    }
+  }
+  nextView() {
+    if (this.state.i === 0) {
+      return "High School";
+    } else if (this.state.i === 1) {
+      return "College";
+    } else if (this.state.i === 2) {
+      return "Grad School";
+    }
+  }
   render() {
     return (
       <div>
         <span>
           <p className="title banner is-1 underline">{this.viewTitle()}</p>
-        </span>
+        </span>{" "}
+        <div className="buttons is-centered columns">
+          {this.state.i >= 1 ? (
+            <div
+              className="button is-primary is-2 column prev-view"
+              onClick={this.viewsDecrement}>
+              &lt;-{this.prevView()}
+            </div>
+          ) : null}
+          <div
+            className={
+              this.state.i === 0 || this.state.i === 3
+                ? "column is-9"
+                : "column is-7"
+            }></div>
+          {this.state.i <= 2 ? (
+            <div
+              className="button is-primary column  is-2 next-view"
+              onClick={this.viewsIncrement}>
+              {this.nextView()} -&gt;
+            </div>
+          ) : null}
+        </div>
         <div className="slide-wrapper">
           {this.state.i === 0 ? <Childhood /> : null}
           {this.state.i === 1 ? <HighSchool /> : null}
           {this.state.i === 2 ? <College /> : null}
-        </div>
-        <div className="buttons columns is-centered">
-          {this.state.i >= 1 ? (
-            <div
-              className="button is-primary column is-1 next-view"
-              onClick={this.viewsDecrement}>
-              &lt;-
-            </div>
-          ) : null}
-          {this.state.i <= 2 ? (
-            <div
-              className="button is-primary column is-1 prev-view"
-              onClick={this.viewsIncrement}>
-              -&gt;
-            </div>
-          ) : null}
         </div>
       </div>
     );
