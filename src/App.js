38be9c8e994d9d5a4d7817cd21272640b9f18001
jwosborn <osbornjeremyw@yahoo.com>
2020-04-03 11:25:09 -0400

    init about and fix app.js imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Beginnings from "./components/Beginnings.js";
-import Home from "./components/Home.js";
-import Menu from "./components/Menu.js";
-import NotFound from "./components/NotFound.js";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import NotFound from "./components/NotFound";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 
@@ -15,6 +16,7 @@ function App() {
           <Switch>
             <Route exact path="/" render={() => <Home />} />
             <Route path="/beginnings" render={() => <Beginnings />} />
+            <Route path="/about" render={() => <About />} />
             <Route path="*" render={() => <NotFound />} />
           </Switch>
         </div>
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div className="section title underline">
+        <p>About Me</p>
+      </div>
+    </div>
+  );
+}
+export default About;
