6c59c89e865cc21069118b1bff59d43734114f86
jwosborn <osbornjeremyw@yahoo.com>
2020-04-06 11:45:18 -0400

    add content copy to dev

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Beginnings from "./components/Beginnings";
 import Home from "./components/Home";
 import About from "./components/About";
+import Dev from "./components/Dev";
 import Menu from "./components/Menu";
 import NotFound from "./components/NotFound";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -17,6 +18,7 @@ function App() {
             <Route exact path="/" render={() => <Home />} />
             <Route path="/beginnings" render={() => <Beginnings />} />
             <Route path="/about" render={() => <About />} />
+            <Route path="/dev" render={() => <Dev />} />
             <Route path="*" render={() => <NotFound />} />
           </Switch>
         </div>
---
+++ src/components/Dev.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Dev() {
+  return (
+    <div>
+      <div className="title is-1 underline">
+        <p>My Dev Journey</p>
+      </div>
+      <div className="content is-medium">
+        <p>
+          As mentioned in my "Where I came from" section, I intend my primary
+          vocation to be a pastor. However, in so many churches, pastors have
+          secular jobs as well. Once I started to realize this, I began to
+          explore some career options that offer flexibility, certainty, as well
+          as fulfillment! I searched for about a year and in late 2018, upon the
+          advice of a good friend, started to explore software development.
+        </p>
+        <p>
+          Like many people, I started with Treehouse to learn the most very
+          basic principles of HTML, CSS, and JavaScript, the bones on which sit
+          much of the internet. Once I had a decent grasp of the basics, I
+          started to build my own apps and explore frameworks and their power.
+          At the time of writing I have experience in Vue.js, React (obviously),
+          Laravel(PHP), and a little Golang.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Dev;
